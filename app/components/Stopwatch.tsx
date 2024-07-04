a18d38194a9a0f681bd70715895232bd5d4e0e55
Andrea Torredimare <andrea@torredimare.it>
2024-07-04 23:22:07 +0200

    Initial commit

---
+++ app/components/Stopwatch.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Stopwatch = () => {
+  return (
+    <div>
+    Stopwatch
+    </div>
+  )
+}
+
+export default Stopwatch
---
+++ app/stopwatch/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Stopwatch from '../components/Stopwatch'
+
+const page = () => {
+  return (
+    <Stopwatch/>
+  )
+}
+
+export default page
